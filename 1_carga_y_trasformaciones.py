9bba79f0c9a01aba867b6f6c27e225656dcc9942
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2025-01-22 19:22:44 -0500

    correccion_transformaciones

--- 1_carga_y_trasformaciones.py
+++ 1_carga_y_trasformaciones.py
@@ -45,24 +45,43 @@ df_bdata.duplicated().sum()
 #Analsis datos nulos
 
 """En este apartado se identifican varios datos nulos en diferentes variables, para su tratamiento se decidio
-    -Eliminar los datos nulos de las variables 'Title', 'categories' y 'authors', ya que son variables que no se pueden imputar
-    -Reemplazar nulos de rating con cero ya que se asume que el libro no tiene rating"""
+    -Eliminar los datos nulos de las variables 'Title', 'categories', 'authors', y 'publisheddate ya que son variables que no se pueden imputar
+    -Reemplazar nulos de rating con cero ya que se asume que el libro no tiene rating
+    -Se realizaron transformaciones de forma en las variables de nombre y categoria"""
 
 df_bdata.isnull().sum()
 
 df_bdata = df_bdata.dropna(subset=['title'])
 df_bdata = df_bdata.dropna(subset=['categories'])
 df_bdata = df_bdata.dropna(subset=['authors'])
+df_bdata = df_bdata.dropna(subset=['publisheddate'])
+
+#Se eliminan las columnas inecesarias para el modelo como imagen, previewlink e infolink
+df_bdata = df_bdata.drop(['image', 'previewlink', 'infolink'], axis = 1)
 
 #2.Transformaciones - books_data
 #2.1.Reemplazo nulos por cero en ratingscount
 df_bdata['ratingscount'] = df_bdata['ratingscount'].fillna(0)
 
-#2.2.Transformacion published Date "se conserva solo el año"
+#2.2.Transformacion publishedsate, categoria y autor 
 
+#Transformacion año
 df_bdata['publisheddate'] = df_bdata['publisheddate'].astype(str)
 df_bdata['publisheddate'] = df_bdata['publisheddate'].apply(lambda x : x.strip())
 df_bdata['publisheddate'] = df_bdata['publisheddate'].apply(lambda x : x[:4])
+
+#Transformacion categoria
+df_bdata['categories'] = df_bdata['categories'].astype(str)
+df_bdata['categories'] = df_bdata['categories'].str.slice(1, -1)
+
+#Transformacion autor
+df_bdata['authors'] = df_bdata['authors'].astype(str)
+df_bdata['authors'] = df_bdata['authors'].str.slice(2, -2)
+df_bdata['authors'] = df_bdata['authors'].replace("'", "", regex=True)
+
+
+# Convirtiendo todo a minúsculas
+df_bdata = df_bdata.apply(lambda x: x.str.lower() if x.dtype == 'object' else x)
 
 #3.Base Books_data limpia
 df_clean_bdata = df_bdata.copy()
@@ -71,9 +90,13 @@ df_clean_bdata = df_bdata.copy()
 
 df_brating.dtypes
 
-#Transformacion de nombres de columnas
+
+#Convertimos variables texto a minuscula
+
+#Convirtiendo todo a minusculas
 
 df_brating.columns = df_brating.columns.str.lower()
+
 
 #4.1.Analisis de duplicados - books_rating
 """En la exploracion de datos duplicados podemos observar que inicialmente 
@@ -86,8 +109,8 @@ df_brating[df_brating.duplicated()]
 #4.2.Analisis de datos nulos -books_rating
 
 """Al analizae los datos nulos se observa que 
-    - la variable price tiene gran parte de sus datos nulos, por lo cual no es representativa y se decide eliminarla. 
-        No se imputan los datos ya que los registros no nulos no alcanzan el 20% de la base de datos
+    - la variable price tiene gran parte de sus datos nulos, a pesar de esto se decide eliminar los nulos y conservar los datos sobrantes, pues este volumen de datos 
+    es suficiente para el analisis y podriamos observar si la variable precio tiene algun tipo de relacion con las reseñas
     - eliminamos los librso que no tienen titulo , y que si los dejamos no podemos unir con la base de datos de books_data
     - Los campos userid y profilename tienen gran cantidad de datos nulos, estas pueden ser reseñas de personas anonimas o de personas
     que eliminaron sus cuentas, por lo que se reemplazan el nulo por anonimo"
@@ -96,12 +119,9 @@ df_brating[df_brating.duplicated()]
 df_brating.isnull().sum()
 
 #Se eliminan datos nulos
+df_brating = df_brating.dropna(subset = ['price'])
 df_brating = df_brating.dropna(subset=['title'])
 df_brating = df_brating.dropna(subset=['review/summary'])
-df_brating = df_brating.dropna(subset=['review/text'])
-
-#Se elimina variable price
-df_brating = df_brating.drop(['price'], axis = 1)
 
 
 #4.4.Transformaciones - books_rating
@@ -109,23 +129,35 @@ df_brating = df_brating.drop(['price'], axis = 1)
 df_brating['user_id'] = df_brating['user_id'].fillna('anonimo')
 df_brating['profilename'] = df_brating['profilename'].fillna('anonimo') 
 
+#Transformacion fecha de publicacion de la reseña
+df_brating['review/time'] = pd.to_datetime(df_brating['review/time'], unit='s') 
+df_brating['review/time'] = df_brating['review/time'].astype(str)
+df_brating['review/time'] = df_brating['review/time'].apply(lambda x : x.strip())
+df_brating['review/time'] = df_brating['review/time'].apply(lambda x : x[:4])
+df_brating = df_brating.rename(columns = {'review/time':'review_year'})
+
+#Conversion a minusculas 
+
+df_brating = df_brating.apply(lambda x: x.str.lower() if x.dtype == 'object' else x)
+
 #5.Base Books_rating limpia
 df_clean_brating = df_brating.copy()
+
 
 """Debido al gran tamaño de la base de datos, tomamos una muestra de 300mil datos para conservar
 el rendimiento del repositorio y no tener problemas en la actualizacion"""
 
-df_brating_reducito_1 = df_clean_brating.sample(frac=0.05, random_state=42) 
-df_brating_reducito_2 = df_clean_brating.sample(frac=0.05, random_state=560)
+df_brating_reducito_1 = df_clean_brating.sample(frac=0.4, random_state=42) 
+df_brating_reducito_2 = df_clean_brating.sample(frac=0.4, random_state=455)
 
 
 #6.Guardar bases de datos limpias
 joblib.dump(df_clean_bdata, 'salidas\\df_clean_bdata.pkl',compress = 7)
 
 
-joblib.dump(df_brating_reducito_1, 'salidas\\df_brating_reducito_1.pkl',compress= 5)
-joblib.dump(df_brating_reducito_2, 'salidas\\df_brating_reducito_2.pkl',compress= 5)
+joblib.dump(df_brating_reducito_1, 'salidas\\df_brating_reducito_1.pkl',compress= 8)
+joblib.dump(df_brating_reducito_2, 'salidas\\df_brating_reducito_2.pkl',compress= 8)
 
 #Confirmamos que el peso por archivo no sea mayor a 100 MB
-peso = os.path.getsize('salidas\\df_clean_bdata.pkl')
+peso = os.path.getsize('salidas\\df_brating_reducito_1.pkl')
 peso_mb = peso/(1024*1024)
---
+++ 2_analisis_exploratorio.py
@@ -0,0 +1,7 @@
+#Librerias utilizadas
+import pandas as pd
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Cargar datos

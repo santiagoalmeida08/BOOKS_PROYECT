65b6189844ddb63e75e647bca0ba22849504c8ec
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2025-01-22 22:47:00 -0500

    datos_actualizados

--- 2_analisis_exploratorio.py
+++ 2_analisis_exploratorio.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Cargar datos
+
+df_books = joblib.load('salidas\\df_clean_bdata.pkl')
+df_books_rating = joblib.load('salidas\\df_brating_reducito_1.pkl')
